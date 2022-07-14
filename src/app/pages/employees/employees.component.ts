ed91deb64d89e7aa8e988090578bb9f85010b391
Evan Kilgore <evankilgore85@gmail.com>
2022-07-14 17:14:46 -0500

    Initial commit

---
+++ src/app/pages/employees/employees.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
